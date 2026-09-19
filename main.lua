0ec89776c7b7ed3ee9bbfc8b706b53103aa74197
dino981 <101268000+dino981@users.noreply.github.com>
2026-09-19 11:55:38 -0400

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,36 @@
+--> GLOBAL VARIABLES <--
+local workspace = game:GetService("Workspace")
+local players = game:GetService("Players")
+local player = players.LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait()
+local humanoid = character.Humanoid
+local hrp = character.HumanoidRootPart
+--> GLOBAL VARIABLES <--
+
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Window = Rayfield:CreateWindow({
+    Name = "Decay GUI",
+    Icon = "globe",
+    LoadingTitle = "Decay GUI",
+    LoadingSubtitle = "by dino981",
+    Theme = "Default",
+
+    DisableRayfieldPrompts = true,
+    DisableBuildWarnings = true,
+
+    ConfigurationSaving = {
+        Enabled  = false,
+        FolderName = nil,
+        FileName = "Nil"
+    },
+
+    KeySystem = false,
+})
+
+local HomeTab = Window:CreateTab("Home", "home")
+HomeTab:CreateLabel("Welcome, " .. player.Name)
+Home:CreateSection("Home Section")
+HomeTab:CreateLabel("Display Name: " .. player.DisplayName)
+HomeTab:CreateLabel("Username: " .. player.Name)
+HomeTab:CreateLabel("User ID: " .. player.UserId)
+HomeTab:CreateLabel("Executor: " .. identifyexecutor() .. " " .. version())
